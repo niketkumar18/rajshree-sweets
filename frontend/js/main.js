3f3e462cc0c660a8802042cf608e424595404e97
Niket Kumar <niketkumar18@gmail.com>
2026-08-15 20:14:19 +0530

    Basic website created

---
+++ frontend/js/main.js
@@ -0,0 +1,9 @@
+/*
+ * Rajshree Sweets
+ * Main JavaScript entry point
+ *
+ * JavaScript functionality will be introduced
+ * progressively during the JavaScript module.
+ */
+
+console.log("Rajshree Sweets frontend loaded.");
